909dcff321539cbb38a97d7c78bdee6a320e0768
paulorobertdasilva23-dot <paulorobertdasilva23@gmail.com>
2025-10-31 16:57:35 +0000

    sim

--- logicaSuperTrunfo_Mestre.c
+++ logicaSuperTrunfo_Mestre.c
@@ -13,8 +13,9 @@ int main() {
   char cidade1[30] = "Maranguape";
   float area1 = 20000;
   float pib1 = 100000;
-   int atributo1, resultado1, exibir_pop1, exibir_pontos_truisticos1;
-  float exibir_pib1, exibir_area1, exibir_dens_pop1;
+  int atributo1, resultado1;
+  float exibir_atributo1_valor1, exibir_atributo1_valor2;
+
 
 
 //variáveis para armazenar os valores da carta 2
@@ -26,9 +27,8 @@ int main() {
   char cidade2[30] = "Maraponga";
   float area2 = 12131;
   float pib2 = 45646;
-  int atributo2, resultado2, exibir_pop2, exibir_pontos_truisticos2;
-  float exibir_pib2, exibir_area2, exibir_dens_pop2;
-  float exibirvalor1, exibirvalor2;
+  int atributo2, resultado2;
+  float exibir_atributo2_valor1, exibir_atributo2_valor2;
     
 
   
@@ -61,8 +61,8 @@ case 1:
 
     printf("Você escolheu população \n\n");
     resultado1 = pop1 > pop2 ? 1:0;
-    exibirvalor1 = pop1;
-    exibirvalor2 = pop2;
+    exibir_atributo1_valor1 = pop1;
+    exibir_atributo1_valor2 = pop2;
 
 
     break;
@@ -71,8 +71,8 @@ case 2:
 
     printf("Você escolheu área \n\n");
     resultado1 = area1 > area2 ? 1:0;
-    exibirvalor1 = area1;
-    exibirvalor2 = area2;
+    exibir_atributo1_valor1 = area1;
+    exibir_atributo1_valor1 = area2;
 
     break;
 
@@ -80,8 +80,8 @@ case 3:
 
     printf("Você escolheu PIB \n\n");
     resultado1 = pib1 > pib2 ? 1:0;
-    exibirvalor1 = pib1;
-    exibirvalor2 = pib2;
+    exibir_atributo1_valor1 = pib1;
+    exibir_atributo1_valor2 = pib2;
 
     break;
 
@@ -89,8 +89,8 @@ case 4:
 
     printf("Você escolheu ponotos turisticos \n\n");
     resultado1 = pontos_turisticos1 > pontos_turisticos2 ? 1:0;
-    exibirvalor1 = pontos_turisticos1;
-    exibirvalor2 = pontos_turisticos2;
+    exibir_atributo1_valor1 = pontos_turisticos1;
+    exibir_atributo1_valor2 = pontos_turisticos2;
 
     break;
 
@@ -98,8 +98,8 @@ case 5:
 
     printf("Você escolheu densidade demográfica \n\n");
     resultado1 = dens_pop1 < dens_pop2 ? 1:0;
-    exibirvalor1 = dens_pop1;
-    exibirvalor2 = dens_pop2;
+    exibir_atributo1_valor1 = dens_pop1;
+    exibir_atributo1_valor2 = dens_pop2;
 
     break;
 
@@ -110,6 +110,7 @@ case 5:
 
     break;
     }    
+
 
    
     printf("Escolha o segundo atributo que quer comparar \n\n");
@@ -135,8 +136,8 @@ case 1:
 
     printf("Você escolheu população\n\n");
     resultado2 = pop1 > pop2 ? 1:0;
-    exibirvalor1 = pop1;
-    exibirvalor2 = pop2;
+    exibir_atributo2_valor1 = pop1;
+    exibir_atributo2_valor2 = pop2;
 
     break;
 
@@ -144,8 +145,8 @@ case 2:
 
     printf("Você escolheu área\n\n");
     resultado2 = area1 > area2 ? 1:0;
-    exibirvalor1 = area1;
-    exibirvalor2 = area2;
+    exibir_atributo2_valor1 = area1;
+    exibir_atributo2_valor2 = area2;
 
     break;
 
@@ -153,8 +154,8 @@ case 3:
 
     printf("Você escolheu PIB\n\n");
     resultado2 = pib1 > pib2 ? 1:0;
-    exibirvalor1 = pib1;
-    exibirvalor2 = pib2;
+    exibir_atributo2_valor1 = pib1;
+    exibir_atributo2_valor2 = pib2;
 
     break;
 
@@ -162,8 +163,8 @@ case 4:
 
     printf("Você escolheu ponotos turisticos \n\n");
     resultado2 = pontos_turisticos1 > pontos_turisticos2 ? 1:0;
-    exibirvalor1 = pontos_turisticos1;
-    exibirvalor2 = pontos_turisticos2;
+    exibir_atributo2_valor1 = pontos_turisticos1;
+    exibir_atributo2_valor2 = pontos_turisticos2;
 
     break;
 
@@ -171,8 +172,8 @@ case 5:
 
     printf("Você escolheu densidade demográfica \n\n");
     resultado2 = dens_pop1 < dens_pop2 ? 1:0;
-    exibirvalor1 = dens_pop1;
-    exibirvalor2 = dens_pop2;
+    exibir_atributo2_valor1 = dens_pop1;
+    exibir_atributo2_valor2 = dens_pop2;
 
     break;
 
@@ -186,13 +187,15 @@ case 5:
 
 //Área para exibição dos resultados
 
-        if (resultado1 < resultado2) {
-    printf("carta %d -> %.1f \n", numero_carta1, exibirvalor1);
-    printf("carta %d -> %.1f \n\n", numero_carta2, exibirvalor2);
+        
+    printf("carta %d -> %.1f \n", numero_carta1, exibir_atributo1_valor1);
+    printf("carta %d -> %.1f \n\n", numero_carta2, exibir_atributo2_valor2);
     
+        if (resultado1 > resultado2) {
+
     printf("Carta %d Venceu!! \n", numero_carta1);
         
-}   else if (resultado1 > resultado2){
+}   else if (resultado1 < resultado2){
     printf("Carta %d Venceu!! \n", numero_carta2);
 
 
@@ -203,18 +206,3 @@ case 5:
 return 0;
 } 
 
-
-
-
-
-
-
-//O nome dos dois países.
- 
-//Os dois atributos usados na comparação.
- 
-//Os valores de cada atributo para cada carta.
- 
-//A soma dos atributos para cada carta.
- 
-//Qual carta venceu (ou se houve empate).
